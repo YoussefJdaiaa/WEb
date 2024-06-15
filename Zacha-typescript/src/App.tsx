7c49b56dae664f05280ec7805c8ab3207ecf8a8b
Your Name <youssef.jdaiaa@gmail.com>
2024-06-15 18:57:49 +0200

    Ajout du dossier Zacha-typescript

---
+++ Zacha-typescript/src/App.tsx
@@ -0,0 +1,42 @@
+import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
+import About from "@/scenes/about";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-black">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <About setSelectedPage={setSelectedPage} />
+  
+   
+  
+  </div>
+  );
+  
+}
+export default App;
---
+++ Zacha-typescript/src/shared/types.ts
@@ -0,0 +1,13 @@
+export enum SelectedPage{
+    Home = "home",
+    About = "about",
+    ShopOnline = "shoponline",
+    ContactUs = "contactus"
+  }
+
+export interface AboutType{
+icon:JSX.Element;
+title: string;
+description: string;
+
+}
